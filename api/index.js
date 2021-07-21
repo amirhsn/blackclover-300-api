0e9af73083baf0e44a7ad6c4e63ca53f852344ec
LAPTOP-M0FKNJEP\LENOVO <amirhusein008@gmail.com>
2021-07-21 12:26:11 +0700

    Commit #2

--- api/index.js
+++ api/index.js
@@ -2,15 +2,28 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+const getResponsePerPage = require('./responses/responsePerPage');
+
+const imageLink = ["https://cdn.statically.io/img/kcast/cdn-image.komikcast.com/wp-content/img/B/Black-Clover/300/001.jpg",
+                        "https://cdn.statically.io/img/kcast/cdn-image.komikcast.com/wp-content/img/B/Black-Clover/300/002.jpg",
+                        "https://cdn.statically.io/img/kcast/cdn-image.komikcast.com/wp-content/img/B/Black-Clover/300/003.jpg",
+                        "https://cdn.statically.io/img/kcast/cdn-image.komikcast.com/wp-content/img/B/Black-Clover/300/004.jpg",
+                        "https://cdn.statically.io/img/kcast/cdn-image.komikcast.com/wp-content/img/B/Black-Clover/300/005.jpg",
+                        "https://cdn.statically.io/img/kcast/cdn-image.komikcast.com/wp-content/img/B/Black-Clover/300/006.jpg",
+                        "https://cdn.statically.io/img/kcast/cdn-image.komikcast.com/wp-content/img/B/Black-Clover/300/007.jpg"];
 
 //automatically for app to use body-parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 //ROUTES
-//import routes
-const getImage = require('./routes/routes');
-//Get image
-app.use('/api', getImage);
+app.get('/api/:id', (req,res)=>{
+    let id = req.params.id;
+    getResponsePerPage.response(
+        id,
+        imageLink[id-1],
+        res
+    );
+});
 
 app.listen(4000);
